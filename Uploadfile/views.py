74ad13f3161165324abe4aa7c3bd514243d1886d
prabhjot kaur <priya998815@gmail.com>
2022-10-03 16:19:33 +0530

    update views.py

--- Uploadfile/views.py
+++ Uploadfile/views.py
@@ -1,3 +1,4 @@
+from email.mime import image
 from .models import Product
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -14,16 +15,23 @@ def upload_data(request):
         serilizer = Productserilizer(prod,many=True)
         return Response(serilizer.data)
 
+
     if request.method == 'POST' :          
-            myfile = request.FILES['myfile']     
-            uploaded_file = pd.read_excel(myfile)
+            excelfile = request.FILES['myfile']  
+            images = request.FILES.getlist('images')
+            uploaded_file = pd.read_excel(excelfile)
             dbframe = uploaded_file
             header = list(dbframe.columns)
-            if header == ['id', 'Name', 'price', 'image', 'imgs', 'gst','amount', 'brand']:   
+            if header == ['id', 'Name', 'price', 'image', 'img', 'gst','amount', 'brand']:   
                         for dbframe in dbframe.itertuples():
-                            obj = Product.objects.create(name=dbframe.Name,price = dbframe.price,image = dbframe.image,img = dbframe.imgs,gst=dbframe.gst,amount=dbframe.amount,brand = dbframe.brand)
-                            print(type(obj))
-                            obj.save()
+                               
+                                for image in images:
+                                    if str(image)==str(dbframe.image):
+                                       upload_img = image
+                                obj = Product.objects.create(name=dbframe.Name,price = dbframe.price,image = upload_img,img=dbframe.img,gst=dbframe.gst,amount=dbframe.amount,brand = dbframe.brand)
+                                obj.save()  
             else:
-                    return Response("file doesn't have required fields")
-            return Response("created Product Data")            
+                return Response("file doesn't have required fields")
+            return Response("created Product Data")     
+
+               
